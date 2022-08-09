aa16fbc4da3540cddf00f06adfb782d9e5a2ed28
Michaela Varberg <michaelavarberg@gmail.com>
2022-08-09 11:02:11 -0600

    created function to capture user input in local storage and print

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,30 @@ var submitButton = document.querySelector("#submit");
 var cardDescription = document.querySelector("#description");
 var cardEdit = document.querySelector("#edit");
 var cardDelete = document.querySelector("#delete");
+
+chooseButton.addEventListener("click", function (event) {
+  event.preventDefault();
+  // write function to pull random chore and display in chosenChore
+});
+
+//Creates JSON object with input from form
+submitButton.addEventListener("click", function (event) {
+  event.preventDefault();
+  var newChore = {
+    chore: choreInput.value,
+    room: roomInput.value,
+    responsible: responsibleInput.value,
+  };
+
+  localStorage.setItem("newChore", JSON.stringify(newChore));
+  createNewChore();
+});
+
+//Makes a card with saved chores
+function createNewChore() {
+  var lastChore = JSON.parse(localStorage.getItem("newChore"));
+
+  if (lastChore !== null) {
+    cardDescription.textContent = lastChore.chore;
+  }
+}
