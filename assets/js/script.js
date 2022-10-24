54a2c0873be004563881ba462f9e1737649ade10
Michaela Varberg <michaelavarberg@gmail.com>
2022-10-24 16:16:43 -0600

    created React front end

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,32 +6,59 @@ var roomInput = document.querySelector("#room-select");
 var responsibleInput = document.querySelector("#responsible");
 var submitButton = document.querySelector("#submit");
 var cardDescription = document.querySelector("#description");
+var cardSubtitle = document.querySelector("#subtitle");
 var cardEdit = document.querySelector("#edit");
 var cardDelete = document.querySelector("#delete");
+
+var cards = [];
 
 chooseButton.addEventListener("click", function (event) {
   event.preventDefault();
   // write function to pull random chore and display in chosenChore
 });
 
+function storeCards() {
+  localStorage.setItem("cards", JSON.stringify(cards));
+}
 //Creates JSON object with input from form
 submitButton.addEventListener("click", function (event) {
   event.preventDefault();
   var newChore = {
-    chore: choreInput.value,
-    room: roomInput.value,
-    responsible: responsibleInput.value,
+    chore: choreInput.value.trim(),
+    room: roomInput.value.trim(),
+    responsible: responsibleInput.value.trim(),
   };
-
-  localStorage.setItem("newChore", JSON.stringify(newChore));
-  createNewChore();
+  localStorage.setItem("newestChore", JSON.stringify(newChore));
+  cards.push(localStorage.getItem("newestChore"));
+  storeCards();
+  renderCards();
 });
 
 //Makes a card with saved chores
-function createNewChore() {
-  var lastChore = JSON.parse(localStorage.getItem("newChore"));
-
-  if (lastChore !== null) {
-    cardDescription.textContent = lastChore.chore;
+function renderCards() {
+  var currentCards = JSON.parse(localStorage.getItem("cards") || "[]");
+  cardsArray = currentCards.split();
+  for (var i = 0; i < cards.length; i++) {
+    localStorage.setItem("currentChore", JSON.parse(cardsArray[i]));
+    var checkPrint = localStorage.getItem("currentChore");
+    console.log(checkPrint);
+    if (currentChore.choice !== null) {
+      cardDescription.textContent = currentChore.chore;
+      cardSubtitle.textContent =
+        "in the " +
+        currentChore.room +
+        " \n" +
+        currentChore.responsible +
+        " is Responsible.";
+    } else {
+      return;
+    }
   }
 }
+
+//TODO:
+//Keep track of multiple chores in local storage
+//Be able to choose random chore from local storage and display
+//Clean up design of new cards
+//Create ability to edit or delete a card from the card
+//Show something on the card when chore is completed
--- assets/py/script.py
+++ assets/py/script.py
@@ -1,77 +1,77 @@
-# from concurrent.futures.process import _chain_from_iterable_of_lists
-# from multiprocessing.connection import deliver_challenge
+from concurrent.futures.process import _chain_from_iterable_of_lists
+from multiprocessing.connection import deliver_challenge
 # from tkinter.messagebox import
 
 
-# class bedroomClass:
-#     name = "Bedroom"
-#     chores = ["Make bed", "Clean"]
+class bedroomClass:
+    name = "Bedroom"
+    chores = ["Make bed", "Clean"]
 
-# class bathroomClass:
-#     name =  "Bathroom"
-#     chores = ["Clear off counter and wipe down", "Clean bathtub", "Sweep and clear off floor", "Wash mirror and sink", "Clean toilet", "Empty trash"]
+class bathroomClass:
+    name =  "Bathroom"
+    chores = ["Clear off counter and wipe down", "Clean bathtub", "Sweep and clear off floor", "Wash mirror and sink", "Clean toilet", "Empty trash"]
 
-# class livingroomClass:
-#     name = "Living Room"
-#     chores = ["Clean off coffee table and wipe down", "Clean off counter and wipe down", "Vaccuum", "Declutter for a cat"]
-# #totalChoresList = [bedroomChoresList, bathroomChoresList, livingRoomChoreList]
+class livingroomClass:
+    name = "Living Room"
+    chores = ["Clean off coffee table and wipe down", "Clean off counter and wipe down", "Vaccuum", "Declutter for a cat"]
+#totalChoresList = [bedroomChoresList, bathroomChoresList, livingRoomChoreList]
 
-# #room_list = [bedroomClass, bathroomClass, livingroomClass]
+#room_list = [bedroomClass, bathroomClass, livingroomClass]
 
-# bedroom = bedroomClass()
-# bathroom = bathroomClass()
-# living_room = livingroomClass()
+bedroom = bedroomClass()
+bathroom = bathroomClass()
+living_room = livingroomClass()
 
-# import random
+import random
 
-# #Asks user a series of prompts and randomly selects chore for them to do. Deletes this chore from the list. 
-# def choresChoice():
-#     bed = input("Do you want a bedroom chore?")
-#     if bed == "yes":
-#         if len(bedroom.chores) == 0:
-#             print("You've completed all bedroom chores!")
-#             choresChoice()
-#         else:
-#             randNum = random.randrange(0,len(bedroom.chores))
-#             print(bedroom.chores[randNum])
-#             deletedItem = bedroom.chores.pop(randNum)
-#             print(bedroom.name)
-#             print(deletedItem + " has been removed from your chores list.")
-#     elif input("Do you want a bathroom chore?") == "yes":
-#         if len(bathroom.chores) == 0:
-#             print("You've completed all bathroom chores!")
-#             choresChoice()
-#         else:
-#             randNum = random.randrange(0,len(bathroom.chores))
-#             print(bathroom.chores[randNum])
-#             deletedItem = bathroom.chores.pop(randNum)
-#             print(bathroom.name)
-#             print(deletedItem + " has been removed from your chores list.")
-#     else:
-#         if len(living_room.chores) == 0:
-#             print("You've completed all living room chores!")
-#             choresChoice()
-#         else: 
-#             print("Well, you're getting a living room chore then!")
-#             randNum = random.randrange(0,len(living_room.chores))
-#             print(living_room.chores[randNum])
-#             deletedItem = living_room.chores.pop(randNum)
-#             print(living_room.name)
-#             print(deletedItem + " has been removed from your chores list.")
+#Asks user a series of prompts and randomly selects chore for them to do. Deletes this chore from the list. 
+def choresChoice():
+    bed = input("Do you want a bedroom chore?")
+    if bed == "yes":
+        if len(bedroom.chores) == 0:
+            print("You've completed all bedroom chores!")
+            choresChoice()
+        else:
+            randNum = random.randrange(0,len(bedroom.chores))
+            print(bedroom.chores[randNum])
+            deletedItem = bedroom.chores.pop(randNum)
+            print(bedroom.name)
+            print(deletedItem + " has been removed from your chores list.")
+    elif input("Do you want a bathroom chore?") == "yes":
+        if len(bathroom.chores) == 0:
+            print("You've completed all bathroom chores!")
+            choresChoice()
+        else:
+            randNum = random.randrange(0,len(bathroom.chores))
+            print(bathroom.chores[randNum])
+            deletedItem = bathroom.chores.pop(randNum)
+            print(bathroom.name)
+            print(deletedItem + " has been removed from your chores list.")
+    else:
+        if len(living_room.chores) == 0:
+            print("You've completed all living room chores!")
+            choresChoice()
+        else: 
+            print("Well, you're getting a living room chore then!")
+            randNum = random.randrange(0,len(living_room.chores))
+            print(living_room.chores[randNum])
+            deletedItem = living_room.chores.pop(randNum)
+            print(living_room.name)
+            print(deletedItem + " has been removed from your chores list.")
             
-#     again = input("Would you like to keep cleaning?")
-#     if again == "yes":
-#         choresChoice()
-#     else:
-#         print("Goodbye")
+    again = input("Would you like to keep cleaning?")
+    if again == "yes":
+        choresChoice()
+    else:
+        print("Goodbye")
 
 
-# choresChoice()
+choresChoice()
 
 
 
-#TO DO:
-# connect python to an html input element
+# TO DO:
+# # connect python to an html input element
 # make new elements in html based on user input
 # make user inputs strings in local storage for one chore
 # change this so that it stores a JSON object
